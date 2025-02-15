dc2d007759117b2e382dc93b4791f5bfc8996a84
Winrose Kiriswa <kiriswawinrose@gmail.com>
2025-02-15 20:07:52 +0300

    admin && farmer

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'navbar', component:NavbarComponent},
+    {path: 'footer', component:FooterComponent}
+];
---
+++ frontend/src/app/farmer/farmerdashboard/farmerdashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-farmerdashboard',
+  imports: [],
+  templateUrl: './farmerdashboard.component.html',
+  styleUrl: './farmerdashboard.component.css'
+})
+export class FarmerdashboardComponent {
+
+}
