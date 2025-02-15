395b132cc70aaee26f1d245e6d4aa8af1cace064
Winrose Kiriswa <kiriswawinrose@gmail.com>
2025-02-16 00:11:31 +0300

    admindashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './admin/register/register.component';
+import { AdmindashboardComponent } from './admin/admindashboard/admindashboard.component';
+import { FarmerdashboardComponent } from './farmer/farmerdashboard/farmerdashboard.component';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
     {path: 'navbar', component:NavbarComponent},
-    {path: 'footer', component:FooterComponent}
-];
+    {path: 'footer', component:FooterComponent},
+    {path: 'register', component:RegisterComponent},
+    {path: 'admindashboard', component:AdmindashboardComponent},
+    {path: 'farmerdashboard', component:FarmerdashboardComponent}
+]
